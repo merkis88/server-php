34a029d64ef5a9f1853c8de8a9b049685c1e0e9c
merkis88 <axuenno88@gmail.com>
2025-05-02 16:46:03 +0700

    fix

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 
 try {
-   //Создаем экземпляр приложения и запускаем его
-   $app = require_once __DIR__ . '/../core/bootstrap.php';
-   $app->run();
+    //Создаем экземпляр приложения и запускаем его
+    $app = require_once __DIR__ . '/../core/bootstrap.php';
+    $app->run();
 } catch (\Throwable $exception) {
-echo '<pre>';
-   print_r($exception);
-   echo '</pre>';
+    echo '<pre>';
+    print_r($exception);
+    echo '</pre>';
 }
