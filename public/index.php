931a73eea2bc7657bb02044183ec1d4f05ef5dd9
merkis88 <axuenno88@gmail.com>
2025-05-05 22:10:00 +0700

    registration

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 use Src\Route;
 
+//Включаем запрет на неявное преобразование типов
+declare(strict_types=1);
+//Включаем сессии на все страницы
+session_start();
+
 Route::add('go', [Controller\Site::class, 'index']);
 Route::add('hello', [Controller\Site::class, 'hello']);
 Route::add('signup', [Controller\Site::class, 'signup']);
