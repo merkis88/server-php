416bab14bc642399fa56672f938576e2552ae4c1
merkis88 <axuenno88@gmail.com>
2025-05-07 16:19:16 +0700

    adding front

--- views/site/login.php
+++ views/site/login.php
@@ -1,13 +1,30 @@
-<h2>Авторизация</h2>
-<h3><?= $message ?? ''; ?></h3>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="/css/login.css">
+</head>
+<body>
 
-<h3><?= app()->auth->user()->name ?? ''; ?></h3>
-<?php
-if (!app()->auth::check()):
-    ?>
-    <form method="post">
-        <label>Логин <input type="text" name="login"></label>
-        <label>Пароль <input type="password" name="password"></label>
-        <button>Войти</button>
-    </form>
-<?php endif;
+<div class="login-container">
+    <h2>Авторизация</h2>
+    <h3><?= $message ?? ''; ?></h3>
+
+    <h3><?= app()->auth->user()->name ?? ''; ?></h3>
+
+    <?php if (!app()->auth::check()): ?>
+        <form method="post">
+            <label>Логин
+                <input type="text" name="login">
+            </label>
+            <label>Пароль
+                <input type="password" name="password">
+            </label>
+            <button type="submit">Войти</button>
+        </form>
+    <?php endif; ?>
+</div>
+
+</body>
+</html>
