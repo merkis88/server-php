775856d8a638950348d51761d54ca86ada47fef4
merkis88 <axuenno88@gmail.com>
2025-05-08 14:22:26 +0700

    visual registration

--- views/site/signup.php
+++ views/site/signup.php
@@ -1,8 +1,28 @@
-<h2>Регистрация нового пользователя</h2>
-<h3><?= $message ?? ''; ?></h3>
-<form method="post">
-    <label>Имя <input type="text" name="name"></label>
-    <label>Логин <input type="text" name="login"></label>
-    <label>Пароль <input type="password" name="password"></label>
-    <button>Зарегистрироваться</button>
-</form>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="/css/register.css">
+</head>
+<body>
+
+<div class="register-container">
+    <h2>Регистрация нового пользователя</h2>
+    <h3><?= $message ?? ''; ?></h3>
+    <form method="post">
+        <label>Имя
+            <input type="text" name="name">
+        </label>
+        <label>Логин
+            <input type="text" name="login">
+        </label>
+        <label>Пароль
+            <input type="password" name="password">
+        </label>
+        <button type="submit">Зарегистрироваться</button>
+    </form>
+</div>
+
+</body>
+</html>
