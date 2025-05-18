7e7ab3b053b8029cd3843f95e669ba326a8d0d83
merkis88 <axuenno88@gmail.com>
2025-05-18 19:19:57 +0700

    start work 6: adding API

--- config/app.php
+++ config/app.php
@@ -18,10 +18,13 @@ return [
         'csrf' => \Middlewares\CSRFMiddleware::class,
         'trim' => \Middlewares\TrimMiddleware::class,
         'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+        'json' => \Middlewares\JSONMiddleware::class,
     ],
-
-
-
-
-
+    //Классы провайдеров
+    'providers' => [
+        'kernel' => \Providers\KernelProvider::class,
+        'route' => \Providers\RouteProvider::class,
+        'db' => \Providers\DBProvider::class,
+        'auth' => \Providers\AuthProvider::class,
+    ],
 ];
